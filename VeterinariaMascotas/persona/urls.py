c2f4d6df7f994f4ab727c96db5e5f6228ba35c2e
nikumaki123 <135075192+nikumaki123@users.noreply.github.com>
2023-12-20 18:20:56 -0300

    push

--- VeterinariaMascotas/persona/urls.py
+++ VeterinariaMascotas/persona/urls.py
@@ -21,9 +21,8 @@ urlpatterns = [
     path('consulta/<int:pk>/edit/', views.consulta_edit, name="consulta_edit"),
     path('consulta/<int:pk>/delete/', views.consulta_delete, name="consulta_delete"),
     path('registro/', views.register, name='registro' ),
-    path('api/personas/', views.lista_personas, name='lista_personas'),
-    path('api/personas/<int:pk>/', views.persona_detail, name='persona_detail'),
+    path('api/persona/', views.lista_personas, name='lista_personas'),
+    path('api/persona/<int:pk>/', views.persona_detail, name='persona_detail'),
 ]
-
 
 
